e1651ba4e77839ff46866143ee956dfe8d31d075
Timo Funke <timo.funke@biotronik.com>
2020-09-24 18:34:12 +0200

    Eradicate space for Caster3

--- Run_Caster_Kaldi.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Runnig Kaldi from Dragonfly CLI
-
-set currentpath=%~dp0
-
-TITLE Caster: Status Window
-python -m dragonfly load _*.py --engine kaldi  --no-recobs-messages
-
-pause 1
